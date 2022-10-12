ac0143045d8158ff04b041c32d1c10bd96c1fa28
marhurst <a02269647@usu.edu>
2022-10-11 22:22:32 -0600

    merge conflict resolution

--- DjangoSkeleton/coffee/urls.py
+++ DjangoSkeleton/coffee/urls.py
@@ -5,18 +5,11 @@ from . import views
 
 app_name = 'coffee'
 urlpatterns = [
-<<<<<<< HEAD
-=======
-	path('index/', views.index, name='index'),
->>>>>>> f7b981b94152cba8534f636711ea8641d2941261
 	path('login/', views.loginPage, name='login'),
     path('register/', views.registerPage, name='register'),
     path('userView/', views.userView, name='userView'),
     path('managerView/', views.managerView, name='managerView'),
-<<<<<<< HEAD
     path('manageEmployees/', views.manageEmployees, name='manageEmployees'),
-=======
->>>>>>> f7b981b94152cba8534f636711ea8641d2941261
     path('inventory/', views.inventory, name='inventory'),
     path('inventory/update/<int:pk>/', views.update_inventory, name="update-inventory")
 ]
