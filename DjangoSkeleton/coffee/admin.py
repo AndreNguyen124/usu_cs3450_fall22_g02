17a884b8c3e8af7c3ad2f04a2a3109e125e4df20
andrenguyen124 <andre.nguyen124@gmail.com>
2022-10-14 02:47:19 -0600

    Added the Drink_item to the admin and forms files

--- DjangoSkeleton/coffee/admin.py
+++ DjangoSkeleton/coffee/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Inventory_Item
+from .models import Drink_Item
 
+admin.site.register(Drink_Item)
 admin.site.register(Inventory_Item)
