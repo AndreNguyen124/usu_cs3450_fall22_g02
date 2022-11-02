641e0f6e5a4c2a88281e1db0b44d218a75515ea4
Emily <emily@PXPs-iMac.local>
2022-11-02 11:16:57 -0600

    Added markup and base price to menu item price, connected price to populateDB

--- DjangoSkeleton/coffee/views.py
+++ DjangoSkeleton/coffee/views.py
@@ -14,6 +14,8 @@ from .forms import InventoryForm, CreateUserForm, DrinkForm, MenuForm
 from .models import Inventory_Item, Price_Markup, Profile, Menu_Item #Drink_Item
 from .forms import InventoryForm, CreateUserForm, PriceMarkupForm, AccountBalanceForm, LogHoursForm, DrinkForm
 
+from decimal import Decimal
+
 
 @unauthenticated_user
 def loginPage(request):
@@ -346,6 +348,8 @@ def product_update(request, pk):
 
 
 # TODO: Implement Add/Remove/Edit Menu Item
+@login_required(login_url='coffee:login')
+@allowed_users(allowed_roles=['Manager'])
 def menuItem(request):
     menu_list = Menu_Item.objects.all()
     context = {
@@ -355,6 +359,8 @@ def menuItem(request):
 
 # TODO
 # addDrinkProduct
+@login_required(login_url='coffee:login')
+@allowed_users(allowed_roles=['Manager'])
 def addMenuItem(request, pk):
     item = Menu_Item.objects.get(id=pk)
     if request.method == 'POST':
@@ -372,6 +378,8 @@ def addMenuItem(request, pk):
     return render(request, 'coffee/menu_add.html', context)
 
 
+@login_required(login_url='coffee:login')
+@allowed_users(allowed_roles=['Manager'])
 def deleteMenuItem(request, pk):
     item = Menu_Item.objects.get(id=pk)
 
@@ -382,6 +390,8 @@ def deleteMenuItem(request, pk):
 
 
 
+@login_required(login_url='coffee:login')
+@allowed_users(allowed_roles=['Manager'])
 def menu_update(request, pk):
     item = Menu_Item.objects.get(id=pk)
 
@@ -402,9 +412,15 @@ def menu_update(request, pk):
 
 def getMenuItemPrice(itemId):
     menuItem = Menu_Item.objects.get(id=itemId)
+    markupDecimal = (Price_Markup.objects.first().markup / 100) + 1
+
     price = 0
     for i in menuItem.Ingredients.all():
         price += i.price
+
+    price = price * Decimal(markupDecimal)
+
+    if price < 7.50: return 7.50
 
     return price
 
