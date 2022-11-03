23b29cab844dc7d24b1c2b9fdbc2e850e73e5291
Karl <A02264961@aggies.usu.edu>
2022-11-03 12:43:32 -0600

    Update and finalize menu items

--- DjangoSkeleton/coffee/migrations/0007_menu_item_order_delete_drink_item_menu_item_order.py
+++ DjangoSkeleton/coffee/migrations/0007_menu_item_order_delete_drink_item_menu_item_order.py
@@ -45,17 +45,63 @@ def populate_db(apps, schema):
     i12 = Inventory_Item(name='Lids', quantity=50, price=0.50)
     i12.save()
 
-    m1 = Menu_Item(name='Caramel Frap', price=6.50)
+    i13 = Inventory_Item(name='Irish Cream Syrup', quantity=20, price=0.50)
+    i13.save()
+
+    i14 = Inventory_Item(name='Half and Half', quantity=20, price=0.40)
+    i14.save()
+
+    i15 = Inventory_Item(name='Espresso Shot', quantity=20, price=0.20)
+    i15.save()
+
+    i16 = Inventory_Item(name='Chai Tea Base', quantity=50, price=0.60)
+    i16.save()
+
+    i17 = Inventory_Item(name='Cinnamon', quantity=50, price=0.10)
+    i17.save()
+
+    i18 = Inventory_Item(name='Peppermint', quantity=50, price=0.10)
+    i18.save()
+
+    m1 = Menu_Item(name='Caramel Frap', price=6.00)
     m1.save()
     m1.Ingredients.add(i1, i2, i5, i12, i10)
 
-    m2 = Menu_Item(name='Mocha Frap', price=6.50)
+    m2 = Menu_Item(name='Mocha Frap', price=6.00)
     m2.save()
     m2.Ingredients.add(i1, i2, i4, i8, i12, i10)
 
-    m3 = Menu_Item(name='Pink Lady', price=6.50)
+    m3 = Menu_Item(name='Pink Lady', price=6.00)
     m3.save()
     m3.Ingredients.add(i1, i2, i3, i10, i11, i12)
+
+    m4 = Menu_Item(name='Gaelic Storm', price=7.00)
+    m4.save()
+    m4.Ingredients.add(i13, i13, i14, i14, i15, i10)
+
+    m5 = Menu_Item(name='Headless Horseman', price=7.00)
+    m5.save()
+    m5.Ingredients.add(i6, i17, i8, i14, i15, i10)
+
+    m6 = Menu_Item(name='TripleX', price=6.50)
+    m6.save()
+    m6.Ingredients.add(i15, i15, i15, i14, i3, i10)
+
+    m7 = Menu_Item(name='Vanilla Ice', price=6.50)
+    m7.save()
+    m7.Ingredients.add(i3, i3, i15, i14, i2, i10)
+
+    m8 = Menu_Item(name='Chai Lee Latte', price=7.50)
+    m8.save()
+    m8.Ingredients.add(i16, i1, i17, i14, i8, i10)
+
+    m9 = Menu_Item(name='Cocoa Mudslide', price=7.50)
+    m9.save()
+    m9.Ingredients.add(i15, i4, i7, i14, i8, i10)
+
+    m10 = Menu_Item(name='Tis the Season', price=5.50)
+    m10.save()
+    m10.Ingredients.add(i17, i4, i6, i15, i18, i10)
 
 
 
