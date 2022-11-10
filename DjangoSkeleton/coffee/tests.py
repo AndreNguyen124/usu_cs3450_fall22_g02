8e49ad7be73d2c86772b2b849ec9357e0c43bbbd
andrenguyen124 <andre.nguyen124@gmail.com>
2022-11-10 03:29:44 -0700

    Creating unit tests

--- DjangoSkeleton/coffee/tests.py
+++ DjangoSkeleton/coffee/tests.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.test import Client
-# Create your tests here.
 from coffee.models import Inventory_Item
 
 
-# TODO:
+# Create your tests here.
+
 
 class Inventory_ItemTestCase(TestCase):
     def setUp(self):
@@ -17,11 +18,30 @@ class URLTests(TestCase):
         response = self.client.get('/login/')
         self.assertEqual(response.status_code, 200)
 
-    # https://docs.djangoproject.com/en/4.1/topics/testing/tools/#:~:text=Overview%20and%20a%20quick%20example%C2%B6
-    def testSignin(self):
-        c = Client()
-        response = c.post('/login/', {'username': 'Test', 'password': 'Test12345'})
+    def test_testRegisterPage(self):
+        response = self.client.get('/register/')
         self.assertEqual(response.status_code, 200)
 
+    def test_testUserViewPage(self):
+        response = self.client.get('/userView/')
+        self.assertEqual(response.status_code, 302)
 
-    # response/post with manager creds.
+    def test_testSignin(self):
+        response = self.client.get('/login/')
+        user = User.objects.create(username='testuser')
+        user.set_password('12345')
+        user.save()
+        c = Client()
+        logged_in = c.login(username='testuser', password='12345')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(logged_in)
+
+    # TODO: Need to determine how to test Redirect
+    # def test_testSigninRedirect(self):
+        # response = self.client.get('/login/')
+        # user = User.objects.create(username='testuser')
+        # user.set_password('12345')
+        # user.save()
+        # c = Client()
+        # logged_in = c.login(username='testuser', password='12345')
+        # self.assertRedirects(response, 'http://127.0.0.1:8000/userView/', 200)
