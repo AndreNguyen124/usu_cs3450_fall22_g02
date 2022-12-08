426c7cf6e2deb4225ac0181e29212861052ef33e
marhurst <a02269647@usu.edu>
2022-12-07 18:28:59 -0700

    Redirect Decorator update

--- DjangoSkeleton/coffee/decorators.py
+++ DjangoSkeleton/coffee/decorators.py
@@ -3,7 +3,15 @@ from django.shortcuts import redirect
 def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect('coffee:userView') #this might cause a problem logically later, test to see
+            if request.user.groups.exists():
+                group = request.user.groups.all()[0].name
+                if group == 'Customer':
+                    return redirect('coffee:userView')
+                elif group == 'Employee':
+                    return redirect('coffee:employeeView')
+                else:
+                    return redirect('coffee:managerView')
+            #return redirect('coffee:userView') #this might cause a problem logically later, test to see
             # (like a manager trying to access /login or /register and being sent to the userView)
         else:
             return view_func(request, *args, **kwargs)
